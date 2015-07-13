147794598cd996995429fb7887567b3d3b8d9573
librasama <54zhengjinqiu@163.com>
2015-07-13 14:01:16 +0800

    用，用上了自己发布的Npm包...做代码统计的...\(^o^)/

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'AUWorkshop' });
+  express.query(req, res, next);
+});
+router.get('/game', function(req, res, next){
+  res.render('game', {title:'宅喵喵游戏'});
+  express.query(req, res, next);
+});
+
+router.get('/comic', function(req, res, next) {
+  res.render('comic', { title: '听音乐' });
+  express.query(req, res, next);
+});
+
+
+module.exports = router;
